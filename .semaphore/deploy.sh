ca5033a52306c1bcba7ed3f2702db4bb58fa26da
Matteo Contrini <matteo@contrini.it>
2021-10-19 15:26:48 +0200

    Fix deploy script

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -5,8 +5,7 @@ set -e
 echo $DOCKER_REGISTRY_TOKEN | docker login --username AWS --password-stdin $DOCKER_REGISTRY_NAME
 
 cd /home/deploy/docker/fibraclick-upload-server
-echo DOCKER_TAG=$DOCKER_TAG > .env
-echo HOSTNAME=$HOSTNAME >> .env
+if grep -q -F 'DOCKER_TAG=' .env ; then sed -i 's/DOCKER_TAG=.*/DOCKER_TAG=$DOCKER_TAG/g' .env; else echo "DOCKER_TAG=$DOCKER_TAG" >> .env; fi
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml -p $CONTAINER_NAME up -d --force-recreate --remove-orphans
 exit
